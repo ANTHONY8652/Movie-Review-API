6929b37f5f6624b172f450bff897e1a3e12fbf3f
ANTHONY8652 <githinjianthony720@gmail.com>
2024-10-24 22:16:19 +0300

    changes

--- Movie_Review_API/settings.py
+++ Movie_Review_API/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_yasg',
     'reviews',
     'users',
@@ -129,5 +130,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
 
 AUTH_USER_MODEL = 'users.User'
--- Movie_Review_API/urls.py
+++ Movie_Review_API/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -23,6 +23,7 @@ from rest_framework import permissions
 schema_view = get_schema_view(
     openapi.Info(
         title = 'Movie Review API',
+        default_version = 'v1.0.0',
         description = 'The Movie Review API allows users to create, read, update, and delete (CRUD) movie reviews. Built with Django and Django REST Framework (DRF), this API enables interaction with a database of user-generated movie reviews.',
         terms_of_service = 'https://google.com/policies/terms',
         contact = openapi.Contact(email='githinjianthony720@gmail.com'),
@@ -36,4 +37,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-with-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-with-ui'),
+    path('api/', include('users.urls'), name='users'),
+    path('api/', include('reviews.urls'), name='reviews'),
 ]
